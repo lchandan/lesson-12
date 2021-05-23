ff96c1257a59462ef637a21a72668aa91a4a5393
Chandan Lakkoju <lchandan@outlook.com>
2021-05-23 15:29:44 +0530

    card component,shop data sections updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 
 import './cart-dropdown.styles.scss';
 
-import CustomButton from '../custom-button/custom-button.component'
+import '../cart-item/cart-item.component';
 
-const CartDropDown = () => (
+import {connect} from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component';
+
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
     
 )
 
-export default CartDropDown;
+const mapStateToProps = (state)=>({
+    cartItems : state.cart.cartItems
+})
+
+export default connect(mapStateToProps, null)(CartDropDown);
